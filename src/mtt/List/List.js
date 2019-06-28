7f3b01de5cf81e7a8381f6761e914b9d186e0590
Simone Lippolis <213991+leeppolis@users.noreply.github.com>
2019-06-28 09:59:31 +0200

    Added distances

--- src/mtt/List/List.js
+++ src/mtt/List/List.js
@@ -1,8 +1,27 @@
 import React, {Component} from 'react';
-
+import Location from '../Location/Location';
 import './List.css';
 
 class List extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      maxDistance: 0,
+    }
+  }
+
+  componentDidUpdate( pProps ) {
+    if ( JSON.stringify(this.props.days) !== JSON.stringify(pProps.days) ) {
+      const locations = this.props.days.flatMap( day => day.locations );
+      const distances = locations.flatMap( location => location.distance.fromRome );
+      console.log( distances );
+      const max = Math.max(...distances);
+      console.log( max );
+      this.setState( { maxDistance: max } );
+    }
+  }
+
   render() {
     return (
       <div className="List">
@@ -15,13 +34,7 @@ class List extends Component {
                 <h2>{day.date}</h2>
                 {
                   day.locations.map(
-                    location => (
-                      <div className="Location" key={location.id}>
-                        <h2>{location.place}</h2>
-                        <h1>{location.title}</h1>
-                        <p>{location.description}</p>
-                      </div>
-                    )
+                    location => <Location key={location.id} location={location} maxDistance={this.state.maxDistance} />
                   )
                 }
               </div>
---
+++ src/mtt/Location/Location.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+import './Location.css';
+
+class Location extends Component {
+
+  progress() {
+    const dist = parseInt(this.props.maxDistance, 10);
+    if ( !Number.isNaN(dist) && dist !== 0 ) {
+      const perc = Math.round( (100 * this.props.location.distance.fromRome) / dist );
+      return (
+        <div className="Progress" style={{width:perc+'%'}}>
+          <div className="Label">🏛{this.props.location.distance.fromRome}Km</div>
+        </div>
+      )
+    }
+    return  null;
+  }
+
+  render() {
+    return (
+      <div className="Location" >
+        <h2>{this.props.location.place}</h2>
+        <div className="Distance">
+          { this.progress() }
+        </div>
+        <h1>{this.props.location.title}</h1>
+        <p>{this.props.location.description}</p>
+      </div>
+    );
+  }
+}
+
+export default Location;
