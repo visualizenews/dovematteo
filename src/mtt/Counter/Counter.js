07a491905262e9aecf5ea07d2bd8d2ddae6d762e
Simone Lippolis <213991+leeppolis@users.noreply.github.com>
2019-07-13 10:02:11 +0200

    Nuovo counter

--- src/mtt/Counter/Counter.js
+++ src/mtt/Counter/Counter.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 
 import './Counter.css';
+import './assets/boat.png';
+import './assets/car.png';
+import './assets/equator.png';
 
 const EQUATOR = 40076;
 const ITALY = 7896;
@@ -27,18 +30,27 @@ class Counter extends Component {
   render() {
     return (
       <div className="Counter">
-        <div className="text">
+        <div className="Text">
           <h3>Dal <em>giorno del suo giuramento</em> davanti al Presidente della Repubblica il nostro eroe <em>ha già percorso</em> almeno</h3>
           <h1>{(new Intl.NumberFormat('it-IT').format(this.state.km))} km<sup><small>*</small></sup></h1>
           <h2>Più o meno...</h2>
-          <div className="cards">
-            <div class="card">
+          <div className="Cards">
+            <div class="Card equator">
+              <div class="image">
+                <span>{this.state.equator}</span>
+              </div>
               <p>Ha fatto <strong>{this.state.equator}</strong><sup><small>*</small></sup> volte il giro del mondo</p>
             </div>
-            <div class="card">
+            <div class="Card boat">
+              <div class="image">
+                <span>{this.state.italy}</span>
+              </div>
               <p>Ha circumnavigato <strong>{this.state.italy}</strong><sup><small>*</small></sup> volte le coste italiane, isole comprese</p>
             </div>
-            <div class="card">
+            <div class="Card car">
+              <div class="image">
+                <span>{this.state.milan}</span>
+              </div>
               <p>Ha percorso <strong>{this.state.milan}</strong><sup><small>*</small></sup> volte la distanza stradale tra Milano e Roma</p>
             </div>
           </div>
