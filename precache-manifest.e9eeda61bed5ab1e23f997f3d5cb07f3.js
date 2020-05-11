dc2e64c9d38c924eeb27cd44f6ff002f192cd43e
Gitlab auto-deploy <213991+leeppolis@users.noreply.github.com>
2020-05-11 08:25:33 +0000

    Ready to deploy

---
+++ precache-manifest.e9eeda61bed5ab1e23f997f3d5cb07f3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f92e4cc044a5fc25e43b9c7d8df9d2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f569b99409ff3c487a28",
+    "url": "/static/css/main.d5734c66.chunk.css"
+  },
+  {
+    "revision": "fea2b54098ec44c7d6cf",
+    "url": "/static/js/2.d68b8f76.chunk.js"
+  },
+  {
+    "revision": "f569b99409ff3c487a28",
+    "url": "/static/js/main.72c78b71.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "69b2d99477d000146a4afcd36ab55100",
+    "url": "/static/media/atlas.69b2d994.png"
+  },
+  {
+    "revision": "938c7c7771d62ac7d932c786945a4469",
+    "url": "/static/media/bassano.938c7c77.png"
+  },
+  {
+    "revision": "d4debf20d83047278bfaed03e0cc9688",
+    "url": "/static/media/boat.d4debf20.png"
+  },
+  {
+    "revision": "19aac98d5da933776d5a5458cad93e98",
+    "url": "/static/media/car.19aac98d.png"
+  },
+  {
+    "revision": "4701755cf32407fe7fa3ed8dfe65a67a",
+    "url": "/static/media/colosseo.4701755c.png"
+  },
+  {
+    "revision": "016e9a86a5f73145d76c0c88acfed034",
+    "url": "/static/media/equator.016e9a86.png"
+  },
+  {
+    "revision": "12355831676cd343690cc2989951eda2",
+    "url": "/static/media/forrest.12355831.png"
+  },
+  {
+    "revision": "2043f339bac7b5999fbb602d989031e9",
+    "url": "/static/media/genova.2043f339.png"
+  },
+  {
+    "revision": "3d7856c4547d8c64fb769fa6ea4869e9",
+    "url": "/static/media/lemans.3d7856c4.png"
+  },
+  {
+    "revision": "b261aefab5cfb1e0645e9c6c91418ca1",
+    "url": "/static/media/milano.b261aefa.png"
+  },
+  {
+    "revision": "2778fb76edbfae4d4492b949c9d66135",
+    "url": "/static/media/roma.2778fb76.png"
+  },
+  {
+    "revision": "722ba50771633a59b542728c42d602f4",
+    "url": "/static/media/vn.722ba507.png"
+  },
+  {
+    "revision": "ea98b36d042f0c9ac641161a579cf483",
+    "url": "/static/media/washington.ea98b36d.png"
+  }
+]);
